f94faa163826b3c93b768a4437c73c71323be849
Vinoth <a.vinothk@outlook.com>
2015-11-06 09:42:36 +0530

    Add chapter5 exercises

---
+++ elixirbro/programming-elixir-exercises/chapter5.exs
@@ -0,0 +1,12 @@
+# list-concat
+
+list_concat = fn (list1, list2) -> list1 ++ list2 end
+
+# Sum function
+
+sum = fn (a, b, c) -> a + b + c end
+
+# Tuple to list function
+
+pair_tuple_to_list = fn (tup1) -> Tuple.to_list(tup1) end
+
