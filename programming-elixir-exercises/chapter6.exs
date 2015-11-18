c3cba6bac02ab614f3b0d7ebefc42814b20dc320
Vinoth <a.vinothk@outlook.com>
2015-11-18 09:15:49 +0530

    Recursive functions

--- programming-elixir-exercises/chapter6.exs
+++ programming-elixir-exercises/chapter6.exs
@@ -5,5 +5,19 @@ defmodule Times do
 
   def triple(n), do: n * 3
 
-  def quadrapule(n), do: Times.double(n) * 2
+  def quadrapule(n), do: double(n) * 2
+end
+
+# Modules and Functions
+
+defmodule Sum do
+  def of(0), do: 0
+
+  def of(n),  do: n + of(n-1)
+end
+
+defmodule Gcd do
+  def of(x, 0), do: x
+
+  def of(x, y), do: of(y, rem(x, y))
 end
